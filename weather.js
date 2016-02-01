e2472b9cabc76ff4884e76cfea50261e78e5a67b
A.J. Riddell <ariddell2017@kellogg.northwestern.edu>
2016-01-31 18:12:00 -0600

    Added More to Forecast

--- weather.js
+++ weather.js
@@ -8,8 +8,13 @@ $(function() {
     var markup= "Current Forecast: " + data.currently.summary;
     var temp= "Current Temperature: " + data.currently.apparentTemperature;
     var tempb= "Tomorrow's Forecast: " + data.daily.data[1].summary + ": " + data.daily.data[1].temperatureMax + " / " + data.daily.data[1].temperatureMin;
+    var tempc= "Two-Day Forecast: " + data.daily.data[2].summary + ": " + data.daily.data[2].temperatureMax + " / " + data.daily.data[2].temperatureMin;
+    var tempd= "Three-Day Forecast: " + data.daily.data[3].summary + ": " + data.daily.data[3].temperatureMax + " / " + data.daily.data[3].temperatureMin;
+
     $('.current-temp').html(temp);
     $('.tomorrow-weather').html(tempb);
+    $('.2day-weather').html(tempc);
+    $('.3day-weather').html(tempd);
         // End of your code
 
     $('.weather-report').html(markup);
